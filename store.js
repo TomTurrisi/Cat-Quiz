62a3a66898bb3fe9e87287725656ea4f968047c9
TomTurrisi <57738169+TomTurrisi@users.noreply.github.com>
2020-01-31 17:15:14 -0500

    Add files via upload

---
+++ store.js
@@ -0,0 +1,133 @@
+const myQuestions = [
+    {
+      //Question1
+      question: "How many cat breeds are there in the world?",
+      answers: [
+        "There are 70 breeds of cats in the world",
+        "There are 100 breeds of cats in the world",
+        "There are 40 breeds of cats in the world",
+        "There are 90 breeds of cats in the world",
+      ],
+      correctAnswer:
+        "There are 70 breeds of cats in the world"
+    },
+    {
+      //Question2
+      question: "How many cats are there in the world?",
+      answers: [
+        "There are 100-200 million cats in the world",
+        "There are 400-600 million cats in the world",
+        "There are 200-600 million cats in the world",
+        "There are 300-700 million cats in the world",
+      ],
+      correctAnswer:
+        "There are 200-600 million cats in the world"
+    },
+    {
+      //Question3
+      question: "The heaviest known cat weighed nearly how many pounds?",
+      answers:[
+       "25 Pounds",
+        "35 Pounds",
+        "60 Pounds",
+        "50 Pounds",
+      ],
+      correctAnswer:
+        "50 Pounds"
+    },
+    {
+      //Question4
+      question: "Where do cats rank as a popular pet in the U.S.A.",
+      answers: [
+        "5TH Most popular pet in the U.S.A.",
+        "2ND Most popular pet in the U.S.A.",
+        "1ST Most popular pet in the U.S.A.",
+        "3RD Most popular pet in the U.S.A.",
+      ],
+      correctAnswer:
+        "2ND Most popular pet in the U.S.A."
+    },
+    {
+      //Question5
+      question: "How old can cats live for?",
+      answers: [
+        "10-13 Years",
+        "12-15 Years",
+        "9-16 Years",
+        "7-10 Years",
+      ],
+      correctAnswer:
+        "12-15 Years"
+    },
+    {
+      //Question6
+      question: "The oldest known cat named Cream Puff lived for how many years?",
+      answers: [
+        "38 Years old",
+        "15 Years old",
+        "24 Years old",
+        "20 Years old",
+      ],
+      correctAnswer:
+        "38 Years old"
+    },
+    {
+      //Question7
+      question: "When was the first cat video uploaded to youtube?",
+      answers: [
+        "2006",
+        "2007",
+        "2005",
+        "2004",
+      ],
+      correctAnswer:
+         "2005"
+    },
+    {
+      //Question8
+      question: "Do cats dream like humans?",
+      answers: [
+        "Ture",
+        "False",
+      ],
+      correctAnswer:
+        "True"
+    },
+    {
+      //Question9
+      question: "Are the cats wiskers as wide as their body?",
+      answers: [
+        "True",
+        "False",
+      ],
+      correctAnswer:
+        "Ture"
+    },
+    {
+      //Question10
+      question: "How high can cats jump?",
+      answers: [
+        "5 Feet",
+        "6 Feet",
+        "9 Feet",
+        "8 Feet",
+      ],
+      correctAnswer:
+        "5 Feet"
+    },
+    {
+      //Question11
+      question: "Are all cat born with brown eyes?",
+      answers: [
+        "False",
+        "True",
+      ],
+      correctAnswer:
+        "False"
+    }
+  ];
+  
+  //variables to store the quiz score and question number information
+  var score = 0;
+  var current = 0;
+  
